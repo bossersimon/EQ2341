fixed broadcast error

Assignment_1/PattRecClasses/MarkovChain.py
@@ -132,10 +132,10 @@ class MarkovChain:
     # pX proportional to the state-conditional probability mass or density values for each state and each frame in the observed feature sequence.
         # init
 
-        alfaTemp = np.array( np.multiply( self.q, pX[:,0]) ) # q_j@b_j(x_t) 
+        alfaTemp = np.multiply( self.q, pX[:,0]).T # q_j@b_j(x_t) 
         c = np.array( sum(alfaTemp) )
         alfaHat = np.zeros([self.nStates,pX.shape[1]])
-        alfaHat[:,0] = alfaTemp/c 
+        alfaHat[:,0] = np.reshape(alfaTemp, self.nStates)/c 
 
         # forward step
         for t in range(1,len(pX[0])): 